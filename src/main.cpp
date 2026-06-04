91f6ba06033b4701b44e09adb68a9c75b15f1625
Charles Hayes <cfhayes16@gmail.com>
2026-06-03 20:57:23 -0600

    Added temporary code in main.cpp to test database connection

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,22 @@
 #include <QDebug>
 
 #include "MainWindow.h"
+#include "db/DatabaseManager.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    DatabaseManager dbManager;
+    if (!dbManager.open()) {
+        qCritical() << "Application startup failed: database could not be opened.";
+        return 1;
+    }
+
+    if (!dbManager.initializeSchema()) {
+        qCritical() << "Application startup failed: database schema could not be initialized.";
+        return 1;
+    }
 
     MainWindow window;
     window.show();
